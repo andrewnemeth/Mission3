963916685cd81c335514036995cff09dc2630ec1
Evan Duffy <elduffy@wpi.edu>
2018-11-29 15:55:50 -0500

    Removed Old Dependency and Added Comments

--- encoder/encode.py
+++ encoder/encode.py
@@ -1,14 +1,19 @@
-import array
 
-wordfile = open("words.nsv", 'r')
-words = [w.replace('\n', '') for w in wordfile.readlines()]
+wordfile = open("words.nsv", 'r') # Open Word List (newline separated value)
+words = [w.replace('\n', '') for w in wordfile.readlines()] # Load the words into an array while stripping the \n from each word
 
+## Encodes a string in our random english word format
+# @param string The string to be encoded
+# @returns The encoded string
 def encode(string):
     ret = ""
     for c in string:
         ret+=words[ord(c)] + " "
     return ret[:len(ret)-1]
 
+## Decodes a string from our random english word format
+# @param string The string to be decoded
+# @returns The decoded string
 def decode(string):
     ret = ""
     for w in string.split(' '):
@@ -16,8 +21,11 @@ def decode(string):
     return ret
 
 
+## 'Test'
 if __name__ == "__main__":
     string = "Hello World"
-    estring = encode(string)
-    print "\"" + estring + "\""
-    print "\"" + decode(estring) + "\""
+    estring = encode(string) # Encode the string
+    print "\"" + estring + "\"" # Print the encoded string
+    destring = decode(estring) # Decode the encoded string
+    print "\"" + destring + "\"" # Print the decode of the encoded string
+    assert string is destring # Make sure the decoded string and the original match
